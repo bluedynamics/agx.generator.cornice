6962d3d2756de50c339fc3005bc301958a0b94b6
Christoph Scheid <c@shri.de>
2013-01-24 16:25:20 +0100

    generated the generator stuff; first go

---
+++ src/agx/generator/cornice/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+def register():
+    """register this generator"""
+    import agx.generator.cornice
+    from agx.core.config import register_generator
+    register_generator(agx.generator.cornice)
