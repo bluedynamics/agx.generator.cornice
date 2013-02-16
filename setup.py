f1edef4bb7a5684175f0138c00b98fb55e6bc2cd
Robert Niederreiter <office@squarewave.at>
2013-02-16 14:31:56 +0100

    basics

---
+++ setup.py
@@ -0,0 +1,48 @@
+# 
+
+import os
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+
+version = "1.0"
+shortdesc = "a generator for rest api services using cornice"
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+
+setup(name="agx.generator.cornice",
+      version=version,
+      description=shortdesc,
+      long_description=longdesc,
+      classifiers=[
+          "",
+      ],
+      keywords="",
+      author="christoph scheid",
+      author_email="",
+      url="",
+      license="",
+      packages=find_packages("src"),
+      package_dir={"": "src"},
+      namespace_packages=["agx", "agx.generator"],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          ##code-section dependencies
+          ##/code-section dependencies
+      ],
+      extras_require=dict(
+          ##code-section extras_require
+          ##/code-section extras_require
+      ),
+      entry_points="""
+      ##code-section entry_points
+      
+      ##/code-section entry_points
+      """,
+      ##code-section additionals
+      ##/code-section additionals
+      )
--- src/agx/generator/cornice/__init__.py
+++ src/agx/generator/cornice/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 def register():
-    """register this generator"""
+    """register this generator
+    """
     import agx.generator.cornice
     from agx.core.config import register_generator
     register_generator(agx.generator.cornice)
