b9b3e57354a933fa059d29a5b32a2d776d400bcf
Christoph Scheid <c@shri.de>
2013-03-21 14:29:26 +0100

    newly generated stuff: regenerated the generator

--- src/agx/generator/cornice/scope.py
+++ src/agx/generator/cornice/scope.py
@@ -16,7 +16,6 @@ from node.ext.uml.interfaces import (
     IAssociation,
 )
 
-
 class PackagesAndEggsScope(Scope):
 
     def __call__(self, node):
@@ -24,8 +23,7 @@ class PackagesAndEggsScope(Scope):
             or node.stereotype('pyegg:pymodule') is not None
 
 registerScope('packagesandeggs', 'uml2fs', [IPackage], PackagesAndEggsScope)
-registerScope('classesandpackages', 'uml2fs', [IClass, IPackage], Scope)
-
+registerScope('classesandpackages', 'uml2fs', [IClass,IPackage], Scope)
 
 class cornicescope(Scope):
 
@@ -34,7 +32,6 @@ class cornicescope(Scope):
 
 registerScope('cornicescope', 'uml2fs', None, cornicescope)
 
-
 class ServiceScope(Scope):
 
     def __call__(self, node):
@@ -42,10 +39,18 @@ class ServiceScope(Scope):
 
 registerScope('cornice_service', 'uml2fs', [IClass], ServiceScope)
 
-
 class GETScope(Scope):
 
     def __call__(self, node):
         return node.stereotype('cornice:get') is not None
 
 registerScope('getscope', 'uml2fs', [IOperation], GETScope)
+registerScope('putscope', 'uml2fs', None, Scope)
+
+class POSTScope(Scope):
+
+    def __call__(self, node):
+        return 
+
+registerScope('postscope', 'uml2fs', None, POSTScope)
+registerScope('deletescope', 'uml2fs', None, Scope)
