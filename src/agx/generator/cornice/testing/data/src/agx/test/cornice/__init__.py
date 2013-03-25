9250e5f83ca2dfe6c012e5eda096d24ff9b915f2
Christoph Scheid <c@shri.de>
2013-03-25 19:33:31 +0100

    removing files created by tests

--- src/agx/generator/cornice/testing/data/src/agx/test/cornice/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from agx.test.cornice.myrestservice import MyRestService
-
-def register():
-    """register this generator
-    """
-    import agx.test.cornice
-    from agx.core.config import register_generator
-    register_generator(agx.test.cornice)
