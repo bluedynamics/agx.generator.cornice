b9337b5fe746f6b7183396ced52b7971279f36b1
Philipp Auersperg-Castell <phil@bluedynamics.com>
2013-04-22 21:27:48 +0200

    cleanup

--- src/agx/generator/cornice/generator.py
+++ src/agx/generator/cornice/generator.py
@@ -21,7 +21,7 @@ from agx.core.util import (
     write_source_to_target_mapping,
 )
 
-#from agx.core.interfaces import IScope
+# from agx.core.interfaces import IScope
 
 from agx.core import (
     handler,
@@ -29,13 +29,28 @@ from agx.core import (
 )
 from node.ext.python import Decorator
 from node.ext.python import Module
+from node.ext.python import Attribute
+from node.ext.python.utils import Imports
+from node.ext.uml.utils import TaggedValues
 
-#from node.ext.python.interfaces import IModule
+# from node.ext.python.interfaces import IModule
 
 DEBUG = True
 
-@handler('create_service', 'uml2fs', 'hierarchygenerator', 'cornice_service',
-         order=27)
+def getservicename(source):
+    tgv = TaggedValues(source)
+    name = tgv.direct('name','cornice:service', source.name.lower())
+
+    return name
+
+def getservicepath(source):
+    tgv = TaggedValues(source)
+    name = tgv.direct('name', 'cornice:service', source.name.lower())
+    path = tgv.direct('path', 'cornice:service', '/' + name)
+
+    return path
+
+@handler('create_service', 'uml2fs', 'hierarchygenerator', 'cornice_service')
 def create_service(self, source, target):
     """create a module 'services.py' to hold the cornice services
     
@@ -45,186 +60,86 @@ def create_service(self, source, target):
     and set it to a Service(with parameters)
     parameters are name = name of the
     """
-    #klass=read_target_node(source,target.target)
-    #module=klass.parent
-    if DEBUG:  # pragma: no cover
-        print "============= handler to create a Cornice Service ======="
-        print "handling UML::Class %s" % source.name
-    
-    #create (or reuse) a special module named 'services.py'
-    #for cornice services
-    
-    if not 'services.py' in target.anchor.parent:  # i.e. not already in target
-        # the file has not been created yet
-        if DEBUG:  # pragma: no cover
-            print "services.py not found: creating it."
-        from node.ext.python import Module
-        servicesmodule = Module()  # create a file
-        target.anchor.parent['services.py'] = servicesmodule
-        if DEBUG:  # pragma: no cover
-            print("here is the actual "
-                  "servicesmodule.uuid: %s" % servicesmodule.uuid)
-            print("and servicesmodule.path: %s" % servicesmodule.path)
-        # add a docstring
-        from node.ext.python import Docstring
-        docs = Docstring()
-        docs.text = u'This module contains cornice services'
-        servicesmodule['docstring-0'] = docs
-    else:
-        # the file already exists
-        if DEBUG:  # pragma: no cover
-            print "services.py exists. using it."
-        servicesmodule = target.anchor.parent['services.py']
+    klass = read_target_node(source, target.target)
+    module = klass.parent
     
     # create imports for cornice service
-    from node.ext.python.utils import Imports
-    imps = Imports(servicesmodule)
+    imps = Imports(module)
     imps.set('cornice', 'Service')  # from cornice import Service
     
     # prepare for later: get name of the service
-    servicename = source.name  # tgt.classname.lower()
-    # XXX TODO: the service name should be extracted from the model!
-    # there are tagged values 'name' and 'path'
-    # as long as they don't have values in the model, use default ones
-    #import pdb; pdb.set_trace()
+    servicename = getservicename(source)
+    servicepath = getservicepath(source)
     
-    # # create an Attribute that will define a cornice service
-    from node.ext.python import Attribute
+    # create an Attribute that will define a cornice service
     serviceattr = Attribute()
-    serviceattr.targets = [servicename]  # name of attribute: Classname.lower()
-    serviceattr.value = 'Service(name="foo", path="bar")'
-    # # TODO: extract 'name' and 'path' from model
+    serviceattr.targets = [source.name]  
+    serviceattr.value = 'Service(name="%s", path="%s")' % (servicename, servicepath)
+    serviceattr.__name__ = serviceattr.uuid
     
-    # service attribute is placed in services.py module
-    if not servicesmodule.attributes(source.name):
-        servicesmodule['servicename-attr-' + source.name] = serviceattr
-        #    servicesmodule.insertlineafter(serviceattr, '\n')
-    
-    # use a token to 'remember' the file for writing later?:
-    # the GET, PUT, POST, DELETE handlers need to know where to attach to
-    #from agx.core import token
-    tok = token('cornice_service_module', True, the_uuid=servicesmodule.uuid)
-    tok.the_path = servicesmodule.path
-    print("the uuid: %s") % tok.the_uuid
-    print("the path: %s") % tok.the_path
-    
-    # we need to store the index of child UML::Operations
-    # that have cornice stereotypes on them.
-    # to ind them later they have to go into the token
-    #
-    import node.ext.uml
-    number_of_items = len(source.keys())
-    print("found %s items in %s") % (number_of_items, source.name)
-    for i in range(len(source.keys())):
-        print('%s: %s of type %s') % (i,
-                                      source.keys()[i],
-                                      type(source.values()[i]))
-        if isinstance(source.values()[i], node.ext.uml.classes.Operation):
-            print("--yes, this is an operation. "
-                  "find out if it has a stereotype!")
-            #source.values()[i].printtree()
-            #print("--source.values()[i]:  %s ") % source.values()[i]
-            #print("--source.values()[i].stereotype:('cornice:get')  %s ") % source.values()[i].stereotype('cornice:get')
-            if source.values()[i].stereotype('cornice:get'):
-                print("--found a <<get>>")
-            elif source.values()[i].stereotype('cornice:put'):
-                print("--found a <<put>>")
-            elif source.values()[i].stereotype('cornice:post'):
-                print("--found a <<post>>")
-            elif source.values()[i].stereotype('cornice:delete'):
-                print("--found a <<delete>>")
-            else:
-                print("--found no stereotype.")
-            #print("  %s ") % source.values()[i]
-        #tok.the_operations = ['1', source.child()
-    #(Pdb) source.keys().index('get_api_version')
-    #2
-    
-    
-    # delete the original class from the tree, or at least mark it,
-    # so it will not be generated as python module
-    #
-    # remove the original module from target:
-    # (it was generated because a UML::Class inside a pyegg was found
-    # and turned into a Python module of the same name, lowercase)
-    # I want all 'services' into one module 'services.py'
-    #    target.anchor.parent.detach(source.name.lower() + '.py')
-    
-    # try to end with a newline
-    from node.ext.python import Block
-    bloc = Block('\n')
-    servicesmodule.insertlast(bloc)
-    #servicesmodule['newline-1-' + source.name] = bloc
-    #servicesmodule.insertlast(Block('''
-    #
-    # '''))
-    # too bad -- trailing newlines are forbidden :-/
-    # see devsrc/node.ext.python/src/node/ext/python/parser.py line 231
-    
-    if DEBUG:  # pragma: no cover
-        print "============= end handler create_service ================"
+    # lets insert it after the class definition
+    if not module.attributes(source.name):
+        module.insertafter(serviceattr, klass)
 
-@handler('handle_PUT', 'uml2fs', 'connectorgenerator', 'putscope')
-def handle_PUT(self, source, target):
-    print "==== handler to create a PUT method ===="
-    print "===== end of handler for PUT method ===="
-
-@handler('handle_POST', 'uml2fs', 'connectorgenerator', 'postscope')
-def handle_POST(self, source, target):
-    print "==== handler to create a POST method ===="
-    print "===== end of handler for POST method ===="
-
-@handler('handle_DELETE', 'uml2fs', 'connectorgenerator', 'deletescope')
-def handle_DELETE(self, source, target):
-    print "==== handler to create a DELETE method ===="
-    print "===== end of handler for DELETE method ===="
 
 @handler('reparent_cornice_functions', 'uml2fs', 'connectorgenerator',
          'cornice_service')
 def reparent_cornice_functions(self, source, target):
-    print 'reparenting methods of class ',source.name
-    klass=read_target_node(source,target.target)
-    module=klass.parent
+    print 'reparenting methods of class ', source.name
+    klass = read_target_node(source, target.target)
+    module = klass.parent
     for func in klass.functions():
-        print '    reparenting ',func.functionname
+        print '    reparenting ', func.functionname
         klass.detach(func.name)
-        funcs=module.functions(func.functionname)
+        funcs = module.functions(func.functionname)
         if not funcs:
             module.insertlast(func)
         else:
-            #if already here, remap source/target
-            oldfunc=funcs[0]
-            sourcefunc=source.get(oldfunc.functionname, None)
+            # if already here, remap source/target
+            oldfunc = funcs[0]
+            sourcefunc = source.get(oldfunc.functionname, None)
             if sourcefunc:
-                write_source_to_target_mapping(sourcefunc,func)
+                tok = token(str(sourcefunc.uuid), True, target=oldfunc)
+
+def add_decorator(source, target, decname):
+    tok = token(str(source.uuid), True, target=None)
+    if tok.target:
+        func = tok.target
+    else:
+        func = read_target_node(source, target.target)
+        
+    dec = Decorator(decname)
+    dec.is_callable = True
+    if not func.decorators(decname):
+        func.insertfirst(dec)
 
 @handler('handle_GET', 'uml2fs', 'connectorgenerator', 'getscope')
 def handle_GET(self, source, target):
-    tok = token('cornice_service_module', False)
-    servicemodules_path_in_target = tok.the_path
-    #print("the uuid: %s") % tok.the_uuid
-    print("the path: %s") % tok.the_path
-    
-    path = os.sep.join(servicemodules_path_in_target)
-    print("the path, usable: %s") % path
-    #servicemodule = tok.the_uuid  # does not work. how to get a node by uuid?
-    
-    #
-    #import pdb; pdb.set_trace()
-    # a decorator for the function
-    
-    
-    #dec = Decorator("get." + servicename + '()')
-    #servicemodule['decorator-1'] = dec
-    
-    #print("gave it a decorator: %s") % dec
-    #servicemodule['decorator-1'] = dec
-    #insertbefore(dec, tgt)  # TODO: decorator shall be planted on function
-    print "========= end handler ========="
+    servicename = source.parent.name
+    decname = servicename + ".get"
+    add_decorator(source, target, decname)
+
+@handler('handle_PUT', 'uml2fs', 'connectorgenerator', 'putscope')
+def handle_PUT(self, source, target):
+    servicename = source.parent.name
+    decname = servicename + ".put"
+    add_decorator(source, target, decname)
+
+@handler('handle_POST', 'uml2fs', 'connectorgenerator', 'postscope')
+def handle_POST(self, source, target):
+    servicename = source.parent.name
+    decname = servicename + ".post"
+    add_decorator(source, target, decname)
+
+@handler('handle_DELETE', 'uml2fs', 'connectorgenerator', 'deletescope')
+def handle_DELETE(self, source, target):
+    servicename = source.parent.name
+    decname = servicename + ".delete"
+    add_decorator(source, target, decname)
 
 @handler('purge_cornice_service_class', 'uml2fs', 'semanticsgenerator',
          'cornice_service')
 def purge_cornice_service_class(self, source, target):
-    klass=read_target_node(source,target.target)
-    module=klass.parent
+    klass = read_target_node(source, target.target)
+    module = klass.parent
     module.detach(klass.name)
--- src/agx/generator/cornice/scope.py
+++ src/agx/generator/cornice/scope.py
@@ -38,30 +38,30 @@ class GETScope(Scope):
 
 registerScope('getscope', 'uml2fs', [IOperation], GETScope)
 
-class PUTScope(Scope):
-
-    def __call__(self, node):
-        return 
-
-registerScope('putscope', 'uml2fs', None, PUTScope)
-
-class POSTScope(Scope):
-
-    def __call__(self, node):
-        return 
-
-registerScope('postscope', 'uml2fs', None, POSTScope)
-
-class DELETEScope(Scope):
-
-    def __call__(self, node):
-        return 
-
-registerScope('deletescope', 'uml2fs', None, DELETEScope)
-
 class CorniceMethodScope(Scope):
 
     def __call__(self, node):
         return node.stereotype('cornice:get') is not None or node.stereotype('cornice:put') is not None or node.stereotype('cornice:delete') is not None or node.stereotype('cornice:post') is not None
 
 registerScope('cornice_method', 'uml2fs', [IOperation], CorniceMethodScope)
+
+class PUTScope(Scope):
+
+    def __call__(self, node):
+        return node.stereotype('cornice:put') is not None
+
+registerScope('putscope', 'uml2fs', [IOperation], PUTScope)
+
+class POSTScope(Scope):
+
+    def __call__(self, node):
+        return node.stereotype('cornice:post') is not None
+
+registerScope('postscope', 'uml2fs', [IOperation], POSTScope)
+
+class DELETEScope(Scope):
+
+    def __call__(self, node):
+        return node.stereotype('cornice:delete') is not None
+
+registerScope('deletescope', 'uml2fs', [IOperation], DELETEScope)
